4bf9f820f9f854a5cd4727429e3d6c4b95f588f2
Monika Sagan <msaganx@gmail.com>
2023-07-02 13:27:45 +0200

    Merge branch 'main' of https://github.com/monikasagan/GaNsA

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:gansa/presentation/pages/auth/login/login_page.dart';
 import 'firebase_options.dart';
 import 'presentation/pages/auth/auth_page/auth_page.dart';
 
