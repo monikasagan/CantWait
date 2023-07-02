70affdf8e0a21b5f35f39568ac2716c08a4ee60a
Monika Sagan <msaganx@gmail.com>
2023-07-02 10:53:01 +0200

    Create Auth Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:gansa/presentation/pages/login/login_page.dart';
+import 'package:gansa/presentation/pages/auth/login/login_page.dart';
 import 'firebase_options.dart';
 
 import 'firebase_options.dart';
