a93af454a49898e4cff694c87fd780e242df9d19
Monika Sagan <msaganx@gmail.com>
2023-07-15 21:40:25 +0200

    Improve Ui, use material 13

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData.dark(),
+      theme: ThemeData.dark(useMaterial3: true),
       home: const AuthPage(),
     );
   }
